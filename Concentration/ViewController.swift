ba5f89cca6f2b5bab3e0f96e2592dd167f8b5591
gnun <gnunua@gmail.com>
2019-05-13 15:56:21 +0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Gnun Ulikhanyan on 5/13/19.
+//  Copyright © 2019 Gnun Ulikhanyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
